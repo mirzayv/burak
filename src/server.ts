843baa1ddff28c3131a4bb69b68e472e5218ba70
mirzayv <mirzayv919@gmail.com>
2026-05-26 11:06:47 +0900

    feat: integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
 // Architectural pattern: MVC, Dependency Injection, MVP
-
 // MVC = MODEL VIEW CONTROLLER
-
 // Design pattern: Middleware, Decotar
+
+import dotenv from "dotenv";
+dotenv.config();
+
+/* console.log("PORT:", process.env.PORT);
+
+console.log("MONGO_URL:", process.env.MONGO_URL); */
