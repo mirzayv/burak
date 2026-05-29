aee310b2c9147aa549774145fa208f15c89d53dc
mirzayv <mirzayv919@gmail.com>
2026-05-29 22:18:53 +0900

    fix: modify routing system and controllers

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,11 +1,9 @@
 import { Request, Response } from "express";
 import { T } from "../libs/types/common";
 
-// REACT
+const restaurantController: T = {};
 
-const memberController: T = {};
-
-/* memberController.goHome = (req: Request, res: Response) => {
+restaurantController.goHome = (req: Request, res: Response) => {
   try {
     res.send("Home Page");
   } catch (err) {
@@ -13,7 +11,7 @@ const memberController: T = {};
   }
 };
 
-memberController.getLogin = (req: Request, res: Response) => {
+restaurantController.getLogin = (req: Request, res: Response) => {
   try {
     res.send("Login Page");
   } catch (err) {
@@ -21,12 +19,12 @@ memberController.getLogin = (req: Request, res: Response) => {
   }
 };
 
-memberController.getSignup = (req: Request, res: Response) => {
+restaurantController.getSignup = (req: Request, res: Response) => {
   try {
     res.send("Signup Page");
   } catch (err) {
     console.log("Error, getSignup:", err);
   }
-}; */
+};
 
-export default memberController;
+export default restaurantController;
--- src/router.ts
+++ src/router.ts
@@ -20,10 +20,10 @@ import express from "express";
 const router = express.Router();
 import memberController from "./controllers/member.controller";
 
-router.get("/", memberController.goHome);
+/* router.get("/", memberController.goHome);
 
 router.get("/login", memberController.getLogin);
 
-router.get("/signup", memberController.getSignup);
+router.get("/signup", memberController.getSignup); */
 
 export default router;
