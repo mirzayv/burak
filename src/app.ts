af6b5304e8ffbfc0ce7d3cd5bb9858755db995e3
mirzayv <mirzayv919@gmail.com>
2026-05-29 21:53:21 +0900

    feat: create routing and member controller

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,21 @@
 import express from "express";
 import path from "path";
+import router from "./router";
 
-/** 1-ENTRANCE **/
+/** 1–ENTRANCE **/
 const app = express();
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-/** 2-SESSIONS **/
+/** 2–SESSIONS **/
 
-/** 3-VIEWS **/
+/** 3–VIEWS **/
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-/** 4-ROUTERS **/
+/** 4–ROUTERS **/
+app.use("/", router); //Middleware Design Pattern
 
 export default app;
---
+++ src/router.ts
@@ -0,0 +1,29 @@
+/* import express, { Request, Response } from "express";
+const router = express.Router();
+
+router.get("/", (req: Request, res: Response) => {
+  res.send("Home Page");
+});
+
+router.get("/login", (req: Request, res: Response) => {
+  res.send("Login Page");
+});
+
+router.get("/signup", (req: Request, res: Response) => {
+  res.send("Signup Page");
+});
+
+export default router;
+ */
+
+import express from "express";
+const router = express.Router();
+import memberController from "./controllers/member.controller";
+
+router.get("/", memberController.goHome);
+
+router.get("/login", memberController.getLogin);
+
+router.get("/signup", memberController.getSignup);
+
+export default router;
