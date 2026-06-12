09b122d670f6801bab24b950787bd0779dff329c
mirzayv <mirzayv919@gmail.com>
2026-06-13 03:45:04 +0900

    fix: modify updateChosenProduct business logic

--- src/libs/types/product.ts
+++ src/libs/types/product.ts
@@ -6,6 +6,20 @@ import {
   ProductVolume,
 } from "../enums/product.enum";
 
+export interface Product {
+  _id: ObjectId;
+  productStatus: ProductStatus;
+  productCollection: ProductCollection;
+  productName: string;
+  productPrice: number;
+  productLeftCount: number;
+  productSize: ProductSize;
+  productVolume: ProductVolume;
+  productDesc?: string;
+  productImages: string[];
+  productViews: number;
+}
+
 export interface ProductInput {
   productStatus?: ProductStatus;
   productCollection: ProductCollection;
@@ -19,16 +33,16 @@ export interface ProductInput {
   productViews?: number;
 }
 
-export interface Product {
+export interface ProductUpdateInput {
   _id: ObjectId;
-  productStatus: ProductStatus;
-  productCollection: ProductCollection;
-  productName: string;
-  productPrice: number;
-  productLeftCount: number;
-  productSize: ProductSize;
-  productVolume: ProductVolume;
+  productStatus?: ProductStatus;
+  productCollection?: ProductCollection;
+  productName?: string;
+  productPrice?: number;
+  productLeftCount?: number;
+  productSize?: ProductSize;
+  productVolume?: ProductVolume;
   productDesc?: string;
-  productImages: string[];
-  productViews: number;
+  productImages?: string[];
+  productViews?: number;
 }
--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -1,5 +1,10 @@
+import { shapeIntoMongooseObjectId } from "../libs/config";
 import Errors, { HttpCode, Message } from "../libs/Errors";
-import { Product, ProductInput } from "../libs/types/product";
+import {
+  Product,
+  ProductInput,
+  ProductUpdateInput,
+} from "../libs/types/product";
 import ProductModel from "../schema/Product.model";
 
 class ProductService {
@@ -16,6 +21,24 @@ class ProductService {
   public async createNewProduct(input: ProductInput): Promise<Product> {
     try {
       return await this.productModel.create(input);
+    } catch (err) {
+      console.log(err);
+      throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+    }
+  }
+
+  public async updateChosenProduct(
+    id: string,
+    input: ProductUpdateInput,
+  ): Promise<Product> {
+    try {
+      id = shapeIntoMongooseObjectId(id);
+      const result = await this.productModel
+        .findOneAndUpdate({ _id: id }, input, { new: true })
+        .exec();
+      if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.UPDATE_FAILED);
+
+      return result;
     } catch (err) {
       console.log(err);
       throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
