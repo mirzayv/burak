e088f2ea175973d84a14e5f37355a6f54363f83e
mirzayv <mirzayv919@gmail.com>
2026-06-23 15:41:30 +0900

    fix: admin project refactoring

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -27,8 +27,6 @@ $("#cancel-btn").on("click", () => {
 $(".new-product-status").on("change", async function (e) {
   const id = e.target.id;
   const productStatus = $(`#${id}.new-product-status`).val();
-  console.log("id:", id);
-  console.log("productStatus:", productStatus);
 
   try {
     const response = await axios.post(`/admin/product/${id}`, {
@@ -38,7 +36,6 @@ $(".new-product-status").on("change", async function (e) {
     const result = response.data;
 
     if (result.data) {
-      console.log("Product updated!");
       $(".new-product-status").blur();
     } else alert("Product update failed!");
   } catch (err) {
