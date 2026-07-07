705359305ed03daa806be00222f1920d87bdba65
mirzayv <mirzayv919@gmail.com>
2026-07-07 14:54:28 +0900

    feat: develop getTopUsers rest Api

--- src/router.ts
+++ src/router.ts
@@ -24,6 +24,7 @@ router.post(
   uploader("members").single("memberImage"),
   memberController.updateMember,
 );
+router.get("/member/top-users", memberController.getTopUsers);
 
 /** Product **/
 
