f1a373c0904ee30a58cd583f41d1c63d2842e999
mirzayv <mirzayv919@gmail.com>
2026-07-09 17:12:57 +0900

    fix: modify getProduct business logic

--- src/libs/types/member.ts
+++ src/libs/types/member.ts
@@ -1,10 +1,10 @@
-import { ObjectId } from "mongoose";
+import { Types } from "mongoose";
 import { MemberStatus, MemberType } from "../enums/member.enum";
 import { Request } from "express";
 import { Session } from "express-session";
 
 export interface Member {
-  _id: ObjectId;
+  _id: Types.ObjectId;
   memberType: MemberType;
   memberStatus: MemberStatus;
   memberNick: string;
@@ -36,7 +36,7 @@ export interface LoginInput {
 }
 
 export interface MemberUpdateInput {
-  _id: ObjectId;
+  _id: Types.ObjectId;
   memberStatus?: MemberStatus;
   memberNick?: string;
   memberPhone?: string;
